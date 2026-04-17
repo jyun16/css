b3d76342ece4f12116251a0851445a9687940045
jn <jyun16@gmail.com>
2026-04-17 16:54:06 +0900

    style update

--- gulpfile.js
+++ gulpfile.js
@@ -158,7 +158,7 @@ const rmTrash = (dir, ext, toDir) => {
 
 const cu = {
 	html: cb => {
-		const ignore = [ '^css/', '^js/' ]
+		const ignore = [ '^css/', '^js/', '^fonts/', 'favicon.ico', '^js/select-pure' ]
 		const files = ls('pug').map(x => x.replace(/pug\//, ''))
 		for (let f of ls(CONF.static)) {
 			f = f.replace(CONF.static, '')
